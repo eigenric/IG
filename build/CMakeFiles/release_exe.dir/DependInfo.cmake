1b6de2a5d3bbba6ed35b62e403bdcec545578b6a
Ricardo Ruiz <ricardoruizfdez@gmail.com>
2023-10-02 07:48:35 +0000

    Builds añadidos

---
+++ build/CMakeFiles/release_exe.dir/DependInfo.cmake
@@ -0,0 +1,37 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/eigenric/Library/CloudStorage/Dropbox/dgiim/IG/ig-pracs-plantilla/materiales/src-cpp/cauce.cpp" "CMakeFiles/release_exe.dir/Users/eigenric/Library/CloudStorage/Dropbox/dgiim/IG/ig-pracs-plantilla/materiales/src-cpp/cauce.cpp.o" "gcc" "CMakeFiles/release_exe.dir/Users/eigenric/Library/CloudStorage/Dropbox/dgiim/IG/ig-pracs-plantilla/materiales/src-cpp/cauce.cpp.o.d"
+  "/Users/eigenric/Library/CloudStorage/Dropbox/dgiim/IG/ig-pracs-plantilla/materiales/src-cpp/fbo.cpp" "CMakeFiles/release_exe.dir/Users/eigenric/Library/CloudStorage/Dropbox/dgiim/IG/ig-pracs-plantilla/materiales/src-cpp/fbo.cpp.o" "gcc" "CMakeFiles/release_exe.dir/Users/eigenric/Library/CloudStorage/Dropbox/dgiim/IG/ig-pracs-plantilla/materiales/src-cpp/fbo.cpp.o.d"
+  "/Users/eigenric/Library/CloudStorage/Dropbox/dgiim/IG/ig-pracs-plantilla/materiales/src-cpp/ig-aux.cpp" "CMakeFiles/release_exe.dir/Users/eigenric/Library/CloudStorage/Dropbox/dgiim/IG/ig-pracs-plantilla/materiales/src-cpp/ig-aux.cpp.o" "gcc" "CMakeFiles/release_exe.dir/Users/eigenric/Library/CloudStorage/Dropbox/dgiim/IG/ig-pracs-plantilla/materiales/src-cpp/ig-aux.cpp.o.d"
+  "/Users/eigenric/Library/CloudStorage/Dropbox/dgiim/IG/ig-pracs-plantilla/materiales/src-cpp/lector-jpg.cpp" "CMakeFiles/release_exe.dir/Users/eigenric/Library/CloudStorage/Dropbox/dgiim/IG/ig-pracs-plantilla/materiales/src-cpp/lector-jpg.cpp.o" "gcc" "CMakeFiles/release_exe.dir/Users/eigenric/Library/CloudStorage/Dropbox/dgiim/IG/ig-pracs-plantilla/materiales/src-cpp/lector-jpg.cpp.o.d"
+  "/Users/eigenric/Library/CloudStorage/Dropbox/dgiim/IG/ig-pracs-plantilla/materiales/src-cpp/lector-ply.cpp" "CMakeFiles/release_exe.dir/Users/eigenric/Library/CloudStorage/Dropbox/dgiim/IG/ig-pracs-plantilla/materiales/src-cpp/lector-ply.cpp.o" "gcc" "CMakeFiles/release_exe.dir/Users/eigenric/Library/CloudStorage/Dropbox/dgiim/IG/ig-pracs-plantilla/materiales/src-cpp/lector-ply.cpp.o.d"
+  "/Users/eigenric/Library/CloudStorage/Dropbox/dgiim/IG/ig-pracs-plantilla/src/animacion.cpp" "CMakeFiles/release_exe.dir/Users/eigenric/Library/CloudStorage/Dropbox/dgiim/IG/ig-pracs-plantilla/src/animacion.cpp.o" "gcc" "CMakeFiles/release_exe.dir/Users/eigenric/Library/CloudStorage/Dropbox/dgiim/IG/ig-pracs-plantilla/src/animacion.cpp.o.d"
+  "/Users/eigenric/Library/CloudStorage/Dropbox/dgiim/IG/ig-pracs-plantilla/src/aplicacion-ig.cpp" "CMakeFiles/release_exe.dir/Users/eigenric/Library/CloudStorage/Dropbox/dgiim/IG/ig-pracs-plantilla/src/aplicacion-ig.cpp.o" "gcc" "CMakeFiles/release_exe.dir/Users/eigenric/Library/CloudStorage/Dropbox/dgiim/IG/ig-pracs-plantilla/src/aplicacion-ig.cpp.o.d"
+  "/Users/eigenric/Library/CloudStorage/Dropbox/dgiim/IG/ig-pracs-plantilla/src/camara.cpp" "CMakeFiles/release_exe.dir/Users/eigenric/Library/CloudStorage/Dropbox/dgiim/IG/ig-pracs-plantilla/src/camara.cpp.o" "gcc" "CMakeFiles/release_exe.dir/Users/eigenric/Library/CloudStorage/Dropbox/dgiim/IG/ig-pracs-plantilla/src/camara.cpp.o.d"
+  "/Users/eigenric/Library/CloudStorage/Dropbox/dgiim/IG/ig-pracs-plantilla/src/escena.cpp" "CMakeFiles/release_exe.dir/Users/eigenric/Library/CloudStorage/Dropbox/dgiim/IG/ig-pracs-plantilla/src/escena.cpp.o" "gcc" "CMakeFiles/release_exe.dir/Users/eigenric/Library/CloudStorage/Dropbox/dgiim/IG/ig-pracs-plantilla/src/escena.cpp.o.d"
+  "/Users/eigenric/Library/CloudStorage/Dropbox/dgiim/IG/ig-pracs-plantilla/src/eventos-raton.cpp" "CMakeFiles/release_exe.dir/Users/eigenric/Library/CloudStorage/Dropbox/dgiim/IG/ig-pracs-plantilla/src/eventos-raton.cpp.o" "gcc" "CMakeFiles/release_exe.dir/Users/eigenric/Library/CloudStorage/Dropbox/dgiim/IG/ig-pracs-plantilla/src/eventos-raton.cpp.o.d"
+  "/Users/eigenric/Library/CloudStorage/Dropbox/dgiim/IG/ig-pracs-plantilla/src/eventos-teclado.cpp" "CMakeFiles/release_exe.dir/Users/eigenric/Library/CloudStorage/Dropbox/dgiim/IG/ig-pracs-plantilla/src/eventos-teclado.cpp.o" "gcc" "CMakeFiles/release_exe.dir/Users/eigenric/Library/CloudStorage/Dropbox/dgiim/IG/ig-pracs-plantilla/src/eventos-teclado.cpp.o.d"
+  "/Users/eigenric/Library/CloudStorage/Dropbox/dgiim/IG/ig-pracs-plantilla/src/grafo-escena.cpp" "CMakeFiles/release_exe.dir/Users/eigenric/Library/CloudStorage/Dropbox/dgiim/IG/ig-pracs-plantilla/src/grafo-escena.cpp.o" "gcc" "CMakeFiles/release_exe.dir/Users/eigenric/Library/CloudStorage/Dropbox/dgiim/IG/ig-pracs-plantilla/src/grafo-escena.cpp.o.d"
+  "/Users/eigenric/Library/CloudStorage/Dropbox/dgiim/IG/ig-pracs-plantilla/src/main.cpp" "CMakeFiles/release_exe.dir/Users/eigenric/Library/CloudStorage/Dropbox/dgiim/IG/ig-pracs-plantilla/src/main.cpp.o" "gcc" "CMakeFiles/release_exe.dir/Users/eigenric/Library/CloudStorage/Dropbox/dgiim/IG/ig-pracs-plantilla/src/main.cpp.o.d"
+  "/Users/eigenric/Library/CloudStorage/Dropbox/dgiim/IG/ig-pracs-plantilla/src/malla-ind.cpp" "CMakeFiles/release_exe.dir/Users/eigenric/Library/CloudStorage/Dropbox/dgiim/IG/ig-pracs-plantilla/src/malla-ind.cpp.o" "gcc" "CMakeFiles/release_exe.dir/Users/eigenric/Library/CloudStorage/Dropbox/dgiim/IG/ig-pracs-plantilla/src/malla-ind.cpp.o.d"
+  "/Users/eigenric/Library/CloudStorage/Dropbox/dgiim/IG/ig-pracs-plantilla/src/malla-revol.cpp" "CMakeFiles/release_exe.dir/Users/eigenric/Library/CloudStorage/Dropbox/dgiim/IG/ig-pracs-plantilla/src/malla-revol.cpp.o" "gcc" "CMakeFiles/release_exe.dir/Users/eigenric/Library/CloudStorage/Dropbox/dgiim/IG/ig-pracs-plantilla/src/malla-revol.cpp.o.d"
+  "/Users/eigenric/Library/CloudStorage/Dropbox/dgiim/IG/ig-pracs-plantilla/src/materiales-luces.cpp" "CMakeFiles/release_exe.dir/Users/eigenric/Library/CloudStorage/Dropbox/dgiim/IG/ig-pracs-plantilla/src/materiales-luces.cpp.o" "gcc" "CMakeFiles/release_exe.dir/Users/eigenric/Library/CloudStorage/Dropbox/dgiim/IG/ig-pracs-plantilla/src/materiales-luces.cpp.o.d"
+  "/Users/eigenric/Library/CloudStorage/Dropbox/dgiim/IG/ig-pracs-plantilla/src/objeto3d.cpp" "CMakeFiles/release_exe.dir/Users/eigenric/Library/CloudStorage/Dropbox/dgiim/IG/ig-pracs-plantilla/src/objeto3d.cpp.o" "gcc" "CMakeFiles/release_exe.dir/Users/eigenric/Library/CloudStorage/Dropbox/dgiim/IG/ig-pracs-plantilla/src/objeto3d.cpp.o.d"
+  "/Users/eigenric/Library/CloudStorage/Dropbox/dgiim/IG/ig-pracs-plantilla/src/seleccion.cpp" "CMakeFiles/release_exe.dir/Users/eigenric/Library/CloudStorage/Dropbox/dgiim/IG/ig-pracs-plantilla/src/seleccion.cpp.o" "gcc" "CMakeFiles/release_exe.dir/Users/eigenric/Library/CloudStorage/Dropbox/dgiim/IG/ig-pracs-plantilla/src/seleccion.cpp.o.d"
+  "/Users/eigenric/Library/CloudStorage/Dropbox/dgiim/IG/ig-pracs-plantilla/src/vaos-vbos.cpp" "CMakeFiles/release_exe.dir/Users/eigenric/Library/CloudStorage/Dropbox/dgiim/IG/ig-pracs-plantilla/src/vaos-vbos.cpp.o" "gcc" "CMakeFiles/release_exe.dir/Users/eigenric/Library/CloudStorage/Dropbox/dgiim/IG/ig-pracs-plantilla/src/vaos-vbos.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
